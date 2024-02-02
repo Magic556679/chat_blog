687a46b2210082a4d187f6663481398c6256c5d0
李晏瑋 <magic556679@gmail.com>
2024-02-02 17:00:24 +0800

    Refactor notFound page

--- src/pages/home/NotFound.tsx
+++ src/pages/home/NotFound.tsx
@@ -1,5 +1,20 @@
+import { useRouteError } from 'react-router-dom'
+
 const NotFound = () => {
-  return <>404</>
+  const error: unknown = useRouteError()
+
+  return (
+    <>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>
+          {(error as Error).message ||
+            (error as { statusText?: string }).statusText}
+        </i>
+      </p>
+    </>
+  )
 }
 
 export default NotFound
