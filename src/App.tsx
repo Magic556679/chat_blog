3b5576e3ffbe63c3a8fed01aa690bb67143a1175
李晏瑋 <magic556679@gmail.com>
2024-02-02 16:59:53 +0800

    refactor router structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from '@/pages/home/Index'
+import LoginIndex from '@/pages/login/Index'
+import Login from '@/pages/login/Login'
+import Register from '@/pages/login/Register'
 import NotFound from '@/pages/home/NotFound'
-import Edit from '@/pages/post/Edit'
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/edit/:id', element: <Edit /> },
   {
-    path: '*',
-    element: <NotFound />
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />
+  },
+  {
+    path: '/login',
+    element: <LoginIndex />,
+    children: [
+      {
+        path: '',
+        element: <Login />
+      }
+    ]
+  },
+  {
+    path: '/register',
+    element: <LoginIndex />,
+    children: [
+      {
+        path: '',
+        element: <Register />
+      }
+    ]
   }
 ])
 
